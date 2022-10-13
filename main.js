be4028693f9d952b6e9ca8037a5df41fd0b8db29
camelPhonso <114600712+camelPhonso@users.noreply.github.com>
2022-10-13 08:37:01 +0100

    added main.js to display and manipulate data

--- main.js
+++ main.js
@@ -1,39 +1,58 @@
 //local storage with the given data
-const movieData = {
-    "The Darjeeling Limited": {
-        plot: "A year after their father's funeral, three brothers travel across India by train in an attempt to bond with each other.",
-        cast: ["Jason Schwartzman", "Owen Wilson", "Adrien Brody"],
-        runtime: 151,
-        rating: 7.2,
-        year: 2007,
-      },
-      "The Royal Tenenbaums": {
-        plot: "The eccentric members of a dysfunctional family reluctantly gather under the same roof for various reasons",
-        rating: 7.6,
-        year: 2001,
-        cast: ["Gene Hackman", "Gwnyeth Paltrow", "Anjelica Huston"],
-        runtime: 170,
-      },
-      "Fantastic Mr. Fox": {
-        year: 2009,
-        plot: "An urbane fox cannot resist returning to his farm raiding ways and then must help his community survive the farmers' retaliation.",
-        cast: [
-          "George Clooney",
-          "Meryl Streep",
-          "Bill Murray",
-          "Jason Schwartzman",
-        ],
-        runtime: 147,
-        rating: 7.9,
-      },
-      "The Grand Budapest Hotel": {
-        rating: 8.1,
-        runtime: 159,
-        year: 2014,
-        plot: "A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel's glorious years under an exceptional concierge.",
-        cast: ["Ralph Fiennes", "F. Murray Abraham", "Mathieu Amalric"],
-      }, 
-}
+let movieData = [{
+  "The Darjeeling Limited": {
+    plot: "A year after their father's funeral, three brothers travel across India by train in an attempt to bond with each other.",
+    cast: ["Jason Schwartzman", "Owen Wilson", "Adrien Brody"],
+    runtime: 151,
+    rating: 7.2,
+    year: 2007,
+  },
+  "The Royal Tenenbaums": {
+    plot: "The eccentric members of a dysfunctional family reluctantly gather under the same roof for various reasons",
+    rating: 7.6,
+    year: 2001,
+    cast: ["Gene Hackman", "Gwnyeth Paltrow", "Anjelica Huston"],
+    runtime: 170,
+  },
+  "Fantastic Mr. Fox": {
+    year: 2009,
+    plot: "An urbane fox cannot resist returning to his farm raiding ways and then must help his community survive the farmers' retaliation.",
+    cast: [
+      "George Clooney",
+      "Meryl Streep",
+      "Bill Murray",
+      "Jason Schwartzman",
+    ],
+    runtime: 147,
+    rating: 7.9,
+  },
+  "The Grand Budapest Hotel": {
+    rating: 8.1,
+    runtime: 159,
+    year: 2014,
+    plot: "A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel's glorious years under an exceptional concierge.",
+    cast: ["Ralph Fiennes", "F. Murray Abraham", "Mathieu Amalric"],
+  },
+}];
+
 
 localStorage.setItem('MovieList', JSON.stringify(movieData));
 
+//global variables
+const display = document.getElementById('movie_list');
+
+
+//display the list in #movie_list
+function render(obj){
+  obj.forEach((o) => {
+    //create a paragraph element and its content at each iteration
+    let par = document.createElement('p');
+    let cont = document.createTextNode(o.runtime);
+
+    //append the paragraph and content to their correct parent
+    display.appendChild(par);
+    par.appendChild(cont);
+  });
+};
+
+console.log(object.keys(movieData));
